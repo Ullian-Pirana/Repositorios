7d619e6ed64f6c7c5f8ac6eb26e29952c82733e8
Desocupad0 <senaiullian@gmail.com>
2024-08-02 07:48:13 -0300

    Criação do Menu

--- 2º Semestre/atividade 13/Defs.py
+++ 2º Semestre/atividade 13/Defs.py
@@ -4,3 +4,10 @@ def clean():
     os.system("pause")
     os.system("cls")
 
+def menu():
+
+    print("\t \t Tarefas To-Do \n")
+
+    escolha = (int(input("O que gostaria de fazer? \n 1 - Verificar Tarefas \n 2 - Adicionar tarefa \n 3 - Excluir tarefa \n 4 - Encerrar Programa \n \t ---> ")))
+
+    return escolha
