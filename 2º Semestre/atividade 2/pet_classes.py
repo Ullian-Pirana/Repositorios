2509446bea2556f1f9381a8fbeafc3bc0b80f147
Desocupad0 <senaiullian@gmail.com>
2024-08-30 09:04:21 -0300

    povoamento do arquivo de classes]

--- 2º Semestre/atividade 2/pet_classes.py
+++ 2º Semestre/atividade 2/pet_classes.py
@@ -1,10 +1,61 @@
+import os
+
 class gato:
 
     def __init__(self, nome):
         self.nome = nome
-        
+
+    def parkour(self):
+        print("frente, esquerda, pula, pula, zigzag, derrapa, corre...")
+
+    def alimento(self):
+
+        oque = int(input("vai comer ou beber? \n 1 - comer \n 2 - beber \n --> "))
+        os.system("cls")
+
+        if oque == 1:
+            print("nham nham")
+
+        elif oque == 2:
+            print("glub glub")
+
+        else:
+            print("opção invalida")
+
+    def mudar_nome(self, novo_nome):
+
+        self.nome = novo_nome
+
+        print(self.nome)
 
 class cachorro:
 
-    def __init__(self, nome):
-        self.nome = nome        
+    def __init__(self, nome2):
+        self.nome = nome2        
+
+
+    def latir(self):
+        print("Au Au Au")
+
+    def cagar(self):
+        print("plop")
+
+    def alimento(self):
+
+        oque = int(input("vai comer ou beber? \n 1 - comer \n 2 - beber \n --> "))
+        os.system("cls")
+
+        if oque == 1:
+            print("nham nham")
+
+        elif oque == 2:
+            print("glub glub")
+
+        else:
+            print("opção invalida")
+
+    def mudar_nome(self, novo_nome2):
+
+        self.nome = novo_nome2
+
+        print(self.nome)
