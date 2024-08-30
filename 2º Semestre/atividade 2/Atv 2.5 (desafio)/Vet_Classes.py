5c1b8c097be00017a18b4021d242b38e0e187cf7
Desocupad0 <senaiullian@gmail.com>
2024-08-30 12:43:06 -0300

    criação dos construtores para as duas classes

---
+++ 2º Semestre/atividade 2/Atv 2.5 (desafio)/Vet_Classes.py
@@ -0,0 +1,22 @@
+class Gato:
+    
+    def __init__(self, nome, raca, dono, idade):
+
+        self.nome = nome
+        self.raca = raca
+        self.dono = dono
+        self.idade = idade
+
+
+
+
+
+
+class Cachorro:
+    
+    def __init__(self, nome, raca, dono, idade):
+
+        self.nome = nome
+        self.raca = raca
+        self.dono = dono
+        self.idade = idade
