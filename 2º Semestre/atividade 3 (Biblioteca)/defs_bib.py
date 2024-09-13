24bfbad621a6def7b01923ac8e2403aba41df9f5
Desocupad0 <senaiullian@gmail.com>
2024-09-13 08:12:34 -0300

    Adicionando novo cadastro

--- 2º Semestre/atividade 3 (Biblioteca)/defs_bib.py
+++ 2º Semestre/atividade 3 (Biblioteca)/defs_bib.py
@@ -12,7 +12,7 @@ def menu(): #Função de menu
           1 - Ver livros Disponiveis
           2 - Realizar um emprestimo
           3 - Cadastrar novo livro
-          4- Sair
+          4-  Sair
     """)
     
     escolha = int(input("--> ")) #Váriavel para se inserir um valor
@@ -34,13 +34,13 @@ def ver_livro():
 
         print("\t Todos os Livros: \n ")
         
-        for item, valor in Estante.items(): 
-            print(f"\n {item}: \n")
-            posicao = 1
+        for item, valor in Estante.items():  # Item e valor são variaveis para armazenar os valores no dicionario estante
+            print(f"\n {item}: \n") #imprimir o valor inserido na variavel item
+            posicao = 1 #Numerar os itens na lista 
 
-            for bolha in valor:
-                print(f"{posicao}º - {bolha}")
-                posicao += 1
+            for bolha in valor: #Bolha é a variavel para iterar em valor
+                print(f"{posicao}º - {bolha}") #Imprime a poisção e o item
+                posicao += 1 #Posição vai ser incrementada em 1
 
         print(" ")
 
@@ -64,11 +64,11 @@ def ver_livro():
         #Váriavel para escolher um genero em especifico
         escolha_gen = input("---> ")
 
-        for chave, valor in Estante.items():
-            if chave == "genero":
-                for item in valor:
-                    if item == escolha_gen:
-                        print(f"{chave} : {item}")
+        for chave, valor in Estante.items(): #Chave e valor para armazenar no dicionário estante
+            if chave == "genero": #Valor de chave vai ser igual a genero
+                for item in valor: 
+                    if item == escolha_gen: #Item vai ser o genero inserido na variavel escolha_gen
+                        print(f"{chave} : {item}")# Se a condição for verdadeira vai imprimir valores de chave e item
                     
 
     #Se não ser nenhuma destas opções será inválido
@@ -77,19 +77,20 @@ def ver_livro():
         os.system("pause")
         os.system("cls")
 
-def emprestar(): #Função com o
+def emprestar(): #Função com o objetivo de realizar o empréstimo
 
     print("\t \t Fazer emprestimo \n")
 
-    empre = int(input("O que gostaria de fazer? \n 1 - Realizar novo emprestimo \n 2 - mudar o emprestimo de um cliente \n --> "))
+#Variavel onde o cliente escolhe o que gostaria de realizar /// Opção 1: "Realizar novo emprestimo" - "Mudar o emprestimo de um cliente"
+    empre = int(input("O que gostaria de fazer? \n 1 - Realizar novo emprestimo \n 2 - Mudar o emprestimo de um cliente \n --> "))
 
     os.system("cls")
-
+    #Se escolha for igual a 1 vai fazer um novo emprestimo
     if empre == 1:
-
-        nome = input("Nome do cliente: ")
-        livro = input("Livro a ser emprestado: ")
-        DDE = input("Data de retirada: ")
+        
+        nome = input("Nome do cliente: ") #Inserir nome do cliente
+        livro = input("Livro a ser emprestado: ") # O nome do livro a ser emprestado
+        DDE = input("Data de retirada: ") #DDE = data de empréstimadariter ad aid ,
         DDD = input("Data da Devolução: ")
         contact = input("Forma de contato com o cliente: ")
 
@@ -153,15 +154,20 @@ def emprestar(): #Função com o
         os.system("cls")
 
 def cadastro():
-    while True:
-        print("---- CADASTRO DE LIVROS ----")
-        nome = input("Infome o nome do livro\n -->")
-        genero = input("Infome o gênero do seu livro\n -->")
-        autor = input("Informe o autor do livro \n -->")
-
-        livro = (nome)
-        generoTipo = (genero)
-        autorLivro = (autor)
+    print("---- CADASTRO DE LIVROS ----")
+    nome = input("Informe o nome do livro\n -->")
+    livro = (nome)
         
-        return livro, generoTipo, autorLivro
+    return livro
+def cadastro2():
+    genero = input("Informe o gênero do seu livro\n -->")
+
+    generoTipo = (genero)
+    return generoTipo
+def cadastro3():
+    autor = input("Informe o autor do livro \n -->")
+
+    autorLivro = (autor)
+    return autorLivro
+    
         
