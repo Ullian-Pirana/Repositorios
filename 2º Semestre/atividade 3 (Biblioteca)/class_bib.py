c5a53d124d9d5b1fc8653f36c21a2b115971d548
Desocupad0 <senaiullian@gmail.com>
2024-09-13 07:34:15 -0300

    Adicionando novos defs pafa as novas funcionalidades

--- 2º Semestre/atividade 3 (Biblioteca)/class_bib.py
+++ 2º Semestre/atividade 3 (Biblioteca)/class_bib.py
@@ -4,41 +4,54 @@ import os
 #Classe de emprestimo
 class Cliente:
     #Construtor
-    def __init__(self, NC, livro, DDE, DDD, Contato):
+    def __init__(self, NC, livro, DDE, DDD, Contato, autor, genero):
         
         self.NC = NC #Nome cliente
         self.livro = livro #livro emprestado
         self.DDE = DDE #Data de empréstimo
         self.DDD = DDD #Data de devolução
         self.Contato = Contato #Contanto do usuário
+        self.autor = autor #Nome do autor
+        self.genero = genero #Genero do livro
 
-    def getLivro(self):
-        return self.livro
+    def getLivro(self): #Retornar o  valor 
+        return self.livro 
         
-    def setLivro(self, livro):
+    def setLivro(self, livro): #Adicionar um valor
         self.livro = livro
 
-    def getDDE(self):
+    def getDDE(self): #Retornar o  valor 
         return self.DDE
     
-    def setDDE(self, DDE):
+    def setDDE(self, DDE):#Adicionar um valor
         self.DDE = DDE
         
-    def getDDD(self):
+    def getDDD(self): #Retornar o  valor 
         return self.DDD
     
-    def setDDD(self,DDD):
+    def setDDD(self,DDD): #Adicionar um valor
         self.DDD = DDD
 
-    def getNC(self):
+    def getNC(self):   #Retornar o  valor 
         return self.NC
 
-    def setNC(self, NC):
+    def setNC(self, NC): #Adicionar um valor
         self.NC = NC   
 
-    def getContato(self):
+    def getContato(self):  #Retornar o  valor 
         return self.Contato
     
-    def setContato(self, contato):
+    def setContato(self, contato): #Adicionar um valor
         self.contato = contato
         
+    def setAutor(self, autor): #Adicionar um valor
+        self.autor = autor
+        
+    def setGenero(self, genero): #Adicionar um valor
+        self.genero = genero
+        
+    def getAutor(self):  #Retornar o  valor 
+        return self.autor
+    
+    def getGenero(self):  #Retornar o  valor 
+        return self.genero
