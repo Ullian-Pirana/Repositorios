b2cfec19f1c66eb1e0335da593c41c5aa901d1a7
Desocupad0 <senaiullian@gmail.com>
2024-09-13 14:10:38 -0300

    aplicação de Try/Except em todos os defs

--- 2º Semestre/atividade 4/Defs.py
+++ 2º Semestre/atividade 4/Defs.py
@@ -3,7 +3,7 @@ from classes import Gato, Cachorro
 
 def menu():
     while True:
-        
+
         print("---- SISTEMA DE VETERINARIA ----")
         print("01 - CADASTRAR ANIMAIS")
         print("02 - LISTAR ANIMAIS")
@@ -31,123 +31,168 @@ def cadastro():
         print("02 - GATO")
         print("00 - VOLTAR")
         print("")
-        animal = int(input("Qual opção deseja ? \n--> "))
 
-        if animal == 1:
+        try:
+            animal = int(input("Qual opção deseja ? \n--> "))
+
+        except Exception as e:
+            print(f"Impossivel concluir operação \n erro encontrado: {e}")
+            os.system("pause")
             os.system("cls")
-            print("---- CADASTRO DE CACHORRO ----")
-            nome = input("Infome o nome do seu cachorro\n -->")
-            raca = input("Infome a raça do seu cachorro\n -->")
-            dono = input("Infome o seu nome\n -->")
-            idade = int(input("Infome a idade do seu cachorro\n -->"))
 
-            cachorro = Cachorro(nome,raca,dono,idade)
+        match animal:
+
+            case 1:
+                os.system("cls")
+                print("---- CADASTRO DE CACHORRO ----")
+
+                while True:
+
+                    try:
+                        nome = input("Infome o nome do seu cachorro\n -->")
+                        raca = input("Infome a raça do seu cachorro\n -->")
+                        dono = input("Infome o seu nome\n -->")
+                        idade = int(input("Infome a idade do seu cachorro\n -->"))
+
+                        break
+
+                    except Exception as e:
+                        print(f"Impossivel concluir operação \n erro encontrado: {e}")
+                        os.system("pause")
+                        os.system("cls")
+
+                cachorro = Cachorro(nome,raca,dono,idade)
+                
+                print("")
+                print("CACHORRO CADASTRADO COM SUCESSO")
+                os.system("pause")
+                os.system("cls")
+
+                return cachorro
+
+            case 2:
+
+                os.system("cls")
+                print("---- CADASTRO DE GATO ----")
+
+                while True:
+
+                    try:
+
+                        nome = input("Infome o nome do seu gato\n --> ")
+                        cor = input("Infome a cor do seu gato\n --> ")
+                        dono = input("Infome o seu nome\n --> ")
+                        idade = int(input("Infome a idade do seu gato\n --> "))
+
+                        break
+
+                    except Exception as e:
+
+                        print(f"Impossivel concluir operação \n erro encontrado: {e}")
+                        os.system("pause")
+                        os.system("cls")
+
+                gato = Gato(nome,cor,dono,idade)
+                
+                print("")
+                print("GATO CADASTRADO COM SUCESSO")
+                os.system("pause")
+                os.system("cls")
+
+                return gato
             
-            print("")
-            print("CACHORRO CADASTRADO COM SUCESSO")
-            os.system("pause")
-            os.system("cls")
+            case 0:
 
-            return cachorro
+                print("")
+                os.system("pause")
+                os.system("cls")
+                break
 
-        elif animal == 2:
+            case _:
 
-            os.system("cls")
-            print("---- CADASTRO DE GATO ----")
-            nome = input("Infome o nome do seu gato\n -->")
-            cor = input("Infome a cor do seu gato\n -->")
-            dono = input("Infome o seu nome\n -->")
-            idade = int(input("Infome a idade do seu gato\n -->"))
-
-            gato = Gato(nome,cor,dono,idade)
+                print("")
+                print("OPÇÃO INVALIDA")
+                os.system("pause")
+                os.system("cls")
             
-            print("")
-            print("GATO CADASTRADO COM SUCESSO")
-            os.system("pause")
-            os.system("cls")
-
-            return gato
-        
-        elif animal == 0:
-
-            print("")
-            os.system("pause")
-            os.system("cls")
-            break
-
-        else:
-
-            print("")
-            print("OPÇÃO INVALIDA")
-            os.system("pause")
-            os.system("cls")
-        
 def listar(lista):
+    
     while True:
         print("---- LISTA DE ANIMAIS ----")
         print("01 - LISTAR TODOS OS ANIMAIS")
         print("02 - LISTAR TODOS OS CACHORROS")
         print("03 - LISTAR TODOS OS GATOS")
         print("00 - VOLTAR")
-        escolha = int(input("Qual opção deseja ? \n--> "))
-        os.system("cls")
 
-        if escolha == 1:
+        try:
 
-            print("---- LISTA DE TODOS OS ANIMAIS ----\n")
-            print("ID\tNOME\tIDADE\tESPECIE")
-            
+            escolha = int(input("Qual opção deseja ? \n--> "))
+            os.system("cls")
 
-            cont = 1
-            for animais in lista:
-                print(f"{cont}\t{animais.getNome()}\t{animais.getIdade()}\t{animais.getEspecie()}")
-                cont += 1
+        except Exception as e:
 
-            print("")
+            print(f"Impossivel concluir operação \n erro encontrado: {e}")
             os.system("pause")
             os.system("cls")
 
-        elif escolha == 2:
+        match escolha:
 
-            print("---- LISTA DE TODOS OS CACHORROS ----\n")
-            print("ID\tNOME\tIDADE\tRAÇA\t\tDONO")            
+            case 1:
 
-            cont = 1
-            for animais in lista:
-                if animais.getEspecie() == "Cachorro":
-                    print(f"{cont}\t{animais.getNome()}\t{animais.getIdade()}\t{animais.getRaca()}\t\t{animais.getDono()}")
+                print("---- LISTA DE TODOS OS ANIMAIS ----\n")
+                print("ID\tNOME\tIDADE\tESPECIE")
+                
+
+                cont = 1
+                for animais in lista:
+                    print(f"{cont}\t{animais.getNome()}\t{animais.getIdade()}\t{animais.getEspecie()}")
                     cont += 1
 
-            print("")
-            os.system("pause")
-            os.system("cls")
+                print("")
+                os.system("pause")
+                os.system("cls")
 
-        elif escolha == 3:
-            
-            print("---- LISTA DE TODOS OS GATOS ----\n")
-            print("ID\tNOME\tIDADE\tCOR\t\tDONO")            
+            case 2:
 
-            cont = 1
-            for animais in lista:
-                if animais.getEspecie() == "Gato":
-                    print(f"{cont}\t{animais.getNome()}\t{animais.getIdade()}\t{animais.getCor()}\t\t{animais.getDono()}")
-                    cont += 1
+                print("---- LISTA DE TODOS OS CACHORROS ----\n")
+                print("ID\tNOME\tIDADE\tRAÇA\t\tDONO")            
 
-            print("")
-            os.system("pause")
-            os.system("cls")
+                cont = 1
+                for animais in lista:
+                    if animais.getEspecie() == "Cachorro":
+                        print(f"{cont}\t{animais.getNome()}\t{animais.getIdade()}\t{animais.getRaca()}\t\t{animais.getDono()}")
+                        cont += 1
 
-        elif escolha == 0:
-            print("")
-            os.system("pause")
-            os.system("cls")
-            break
+                print("")
+                os.system("pause")
+                os.system("cls")
 
-        else:
-            print("")
-            print("OPÇÃO INVALIDA")
-            os.system("pause")
-            os.system("cls")
+            case 3:
+                
+                print("---- LISTA DE TODOS OS GATOS ----\n")
+                print("ID\tNOME\tIDADE\tCOR\t\tDONO")            
+
+                cont = 1
+                for animais in lista:
+                    if animais.getEspecie() == "Gato":
+                        print(f"{cont}\t{animais.getNome()}\t{animais.getIdade()}\t{animais.getCor()}\t\t{animais.getDono()}")
+                        cont += 1
+
+                print("")
+                os.system("pause")
+                os.system("cls")
+
+            case  0:
+                print("")
+                os.system("pause")
+                os.system("cls")
+                break
+
+            case _:
+                print("")
+                print("OPÇÃO INVALIDA")
+                os.system("pause")
+                os.system("cls")
 
 def consulta(lista):
     while True:
@@ -155,58 +200,69 @@ def consulta(lista):
         print("01 - DIAGNOSTICAR O ANIMAL")
         print("02 - VERIFICAR DIAGNOSTICO DO ANIMAL")
         print("00 - VOLTAR")
-        escolha = int(input("Qual opção deseja ? \n--> "))
+
+        try:
+
+            escolha = int(input("Qual opção deseja ? \n--> "))
+
+        except Exception as e:
+
+            print(f"Impossivel concluir operação \n erro encontrado: {e}")
+            os.system("pause")
+            os.system("cls")
         
+        match escolha:
 
-        if escolha == 1:
-            print("---- DIAGNOSTICANDO OS ANIMAIS ----")
-            print("")
-            print("ID\tNOME\tESPECIE")       
+            case 1:
+                print("---- DIAGNOSTICANDO OS ANIMAIS ----")
+                print("")
+                print("ID\tNOME\tESPECIE")       
 
-            cont = 1
-            for animais in lista:
-                print(f"{cont}\t{animais.getNome()}\t{animais.getEspecie()}")
-                cont += 1
+                cont = 1
+                for animais in lista:
+                    print(f"{cont}\t{animais.getNome()}\t{animais.getEspecie()}")
+                    cont += 1
 
-            print("")
+                print("")
 
-            id_sel = int(input("Qual animal deseja consultar ? \n--> "))
-            diag = input("informe o diagnostico do animal\n--> ")
+                id_sel = int(input("Qual animal deseja consultar ? \n--> "))
+                diag = input("informe o diagnostico do animal\n--> ")
 
-            lista[id_sel - 1].setDiag(diag)
+                lista[id_sel - 1].setDiag(diag)
 
-            print("")
-            print("DIAGNOSTICO CADASTRADO COM SUCESSO")
-            os.system("pause")
-            os.system("cls")
+                print("")
+                print("DIAGNOSTICO CADASTRADO COM SUCESSO")
+                os.system("pause")
+                os.system("cls")
 
 
-        elif escolha == 2:
+            case 2:
+                
+                print("---- VERIFICANDO DIAGNOSTICO DO ANIMAL ----")
+                print("")
+                print("ID\tNOME\tESPECIE")       
+
+                cont = 1
+                for animais in lista:
+                    print(f"{cont}\t{animais.getNome()}\t{animais.getEspecie()}")
+                    cont += 1
+
+                print("")
+
+                id_sel = int(input("Qual animal deseja verificar ? \n--> "))
+
+                print(f"O diagnostico do animal é: {lista[id_sel - 1].getDiag()}\n")
+                os.system("pause")
+                os.system("cls")
+
+            case 0:
+                print("")
+                os.system("pause")
+                os.system("cls")
+                break
             
-            print("---- VERIFICANDO DIAGNOSTICO DO ANIMAL ----")
-            print("")
-            print("ID\tNOME\tESPECIE")       
-
-            cont = 1
-            for animais in lista:
-                print(f"{cont}\t{animais.getNome()}\t{animais.getEspecie()}")
-                cont += 1
-
-            print("")
-
-            id_sel = int(input("Qual animal deseja verificar ? \n--> "))
-
-            print(f"O diagnostico do animal é: {lista[id_sel - 1].getDiag()}\n")
-            os.system("pause")
-            os.system("cls")
-
-        elif escolha == 0:
-            print("")
-            os.system("pause")
-            os.system("cls")
-            break
-        else:
-            print("")
-            print("OPÇÃO INVALIDA")
-            os.system("pause")
-            os.system("cls")
+            case _:
+                print("")
+                print("OPÇÃO INVALIDA")
+                os.system("pause")
+                os.system("cls")
