663db447e62dc50ee2eca114b4882a7b30b9947e
Desocupad0 <senaiullian@gmail.com>
2024-09-20 10:37:34 -0300

    adição de metodos na classe (raiz) de cadastro

--- 2º Semestre/atividade 5 (Banco)/Classes.py
+++ 2º Semestre/atividade 5 (Banco)/Classes.py
@@ -1,14 +1,68 @@
 class Cadastro:
 
-    def __init__(self, __usuario, __senha, __saldo, __email, __cpf, __rg, __idade, __cep):
+    def __init__(self, usuario, senha, saldo, email, cpf, rg, idade, cep):
 
-        self.usuario = __usuario
-        self.senha = __senha
-        self.saldo = __saldo
-        self.email = __email
-        self.cpf = __cpf
-        self.rg = __rg
-        self.idade = __idade
-        self.cep = __cep
-        
-        
+        self.__usuario = usuario
+        self.__senha = senha
+        self.__saldo = saldo
+        self.__email = email
+        self.__cpf = cpf
+        self.__rg = rg
+        self.__idade = idade
+        self.__cep = cep
+
+    def __str__(self):
+        return self.__usuario
+    
+    #DEF'S para VERIFICAR informações
+    
+    def verSaldo(self):
+        return self.__saldo
+    
+    def verUsuario(self):
+        return self.__usuario
+    
+    def verSenha (self):
+        return self.__senha
+    
+    def verEmail (self):
+        return self.__email
+    
+    def verCpf (self):
+        return self.__cpf
+    
+    def verRg (self):
+        return self.__rg
+    
+    def verIdade (self):
+        return self.__idade
+    
+    def verCep (self):
+        return self.__cep
+    
+    #DEF'S para Mudar as informações
+
+    def setSaldo(self, saldo):
+        self.__saldo = saldo
+    
+    def setUsuario(self, usuario):
+        self.__usuario = usuario
+    
+    def setSenha (self, senha):
+        self.__senha = senha
+    
+    def setEmail (self, email):
+        self.__email = email
+    
+    def setCpf (self, cpf):
+        self.__cpf = cpf
+    
+    def setRg (self, rg):
+        self.__rg = rg
+    
+    def setIdade (self, idade):
+        self.__idade = idade
+    
+    def setCep (self, cep):
+        self.__cep = cep
+
