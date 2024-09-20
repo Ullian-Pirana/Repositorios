9906789d23f6aa58bee608f8301d45cd7b52b211
Desocupad0 <senaiullian@gmail.com>
2024-09-20 12:54:01 -0300

    update

--- 2º Semestre/atividade 5 (Banco)/ULL Bank.py
+++ 2º Semestre/atividade 5 (Banco)/ULL Bank.py
@@ -1,4 +1,13 @@
 import os 
-from Listas import *
 from Classes import *
+from Defs import *
 
+while True:
+    escolha = menu()
+    match escolha:
+        case 1:
+            pass
+        case 2:
+            pass
+        case 3:
+            break
