a50796a8ea1d5c17a123e36b85253f87f28c9692
Desocupad0 <senaiullian@gmail.com>
2024-09-20 10:25:14 -0300

    criação da classe para cadastro

---
+++ 2º Semestre/atividade 5 (Banco)/Classes.py
@@ -0,0 +1,14 @@
+class Cadastro:
+
+    def __init__(self, __usuario, __senha, __saldo, __email, __cpf, __rg, __idade, __cep):
+
+        self.usuario = __usuario
+        self.senha = __senha
+        self.saldo = __saldo
+        self.email = __email
+        self.cpf = __cpf
+        self.rg = __rg
+        self.idade = __idade
+        self.cep = __cep
+        
+        
