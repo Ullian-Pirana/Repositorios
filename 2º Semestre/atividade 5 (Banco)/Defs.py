3efc7b04b64a339fd3d9d1da35785e524d48828f
Desocupad0 <senaiullian@gmail.com>
2024-09-20 15:21:03 -0300

    arrumei o def de menu

--- 2º Semestre/atividade 5 (Banco)/Defs.py
+++ 2º Semestre/atividade 5 (Banco)/Defs.py
@@ -3,27 +3,25 @@ from Listas import *
 from Classes import *
 
 def menu():
-
+    print("--- ULL Bank ---")
+    print("Seja bem vindo ao ULL Bank, um banco especializado em resolver os seus problemas: \nO que deseja fazer? \n 1- Fazer Login \n 2- Fazer cadastro \n 3 - sair")
+    
     while True:
-        print("--- ULL Bank ---")
-        print("Seja bem vindo ao ULL Bank, um banco especializado em resolver os seus problemas: \nO que deseja fazer? \n 1- Fazer Login \n 2- Fazer cadastro \n 3 - sair")
+        try:
+            escolha = int(input("\n Qual opção deseja ? \n ---> "))
+
+            os.system("cls")
+
+            break
+
+        except Exception as e:
+
+            print(f"Impossivel concluir operação \n erro encontrado: {e}")
+
+            os.system("pause")
+            os.system("cls")
         
-        while True:
-            try:
-                escolha = int(input("\n Qual opção deseja ? \n ---> "))
-
-                os.system("cls")
-
-                break
-
-            except Exception as e:
-
-                print(f"Impossivel concluir operação \n erro encontrado: {e}")
-
-                os.system("pause")
-                os.system("cls")
-            
-            return escolha
+    return escolha
 
 def cadastro():
 
