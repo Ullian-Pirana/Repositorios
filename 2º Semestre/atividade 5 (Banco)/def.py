5008dc636301d8344ae6ce28a4f807b65d4ac8ec
Desocupad0 <senaiullian@gmail.com>
2024-09-20 10:48:03 -0300

    importando as informações do arquivo de listas

--- 2º Semestre/atividade 5 (Banco)/def.py
+++ 2º Semestre/atividade 5 (Banco)/def.py
@@ -1,16 +1,24 @@
 import os
+from Listas import *
 
 def menu():
+
     while True:
         print("--- ULL Bank ---")
-        print("Seja bem vindo ao ULL Bank, um banco especializado em resolver os seus problemas: \nO que deseja fazer? \n1- Fazer Login \n2- Fazer cadastro")
+        print("Seja bem vindo ao ULL Bank, um banco especializado em resolver os seus problemas: \nO que deseja fazer? \n 1- Fazer Login \n 2- Fazer cadastro \n 3 - sair")
+        
         while True:
             try:
-                escolha = int(input("Qual opção deseja ? \n--> "))
+                escolha = int(input("Qual opção deseja ? \n ---> "))
+
+                os.system("cls")
+
                 return escolha
                 break
+
             except Exception as e:
                 print(f"Impossivel concluir operação \n erro encontrado: {e}")
+
                 os.system("pause")
                 os.system("cls")
                 return escolha
