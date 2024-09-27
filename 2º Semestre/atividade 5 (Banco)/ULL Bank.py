2888d47bf3188571a257ad629e21a2bd1e923cfa
Desocupad0 <senaiullian@gmail.com>
2024-09-27 12:40:07 -0300

    variacel

--- 2º Semestre/atividade 5 (Banco)/ULL Bank.py
+++ 2º Semestre/atividade 5 (Banco)/ULL Bank.py
@@ -11,30 +11,39 @@ while True:
             user = login(cadastros)
             usar = tela_banco(user)
 
+
+
             match usar:
                 case 1:
-                    while True:
-                        escolha2 = corrente(user)
+                    if cadastros.__len__() > 0:
+                        while True:
+                            escolha2 = corrente(user)
 
-                        match escolha2:
-                            case 1:
-                                deposito(user)
+                            match escolha2:
+                                case 1:
+                                    deposito(user)
 
-                            case 2:
-                                pix(user)
+                                case 2:
+                                    pix(user)
 
-                            case 3:
-                                print("Saindo....")
+                                case 3:
+                                    print("Saindo....")
 
-                                os.system("pause")
-                                os.system("cls")
-                                break
+                                    os.system("pause")
+                                    os.system("cls")
+                                    break
 
-                            case _:
-                                print("Opção invalida...")
+                                case _:
+                                    print("Opção invalida...")
 
-                                os.system("pause")
-                                os.system("cls")
+                                    os.system("pause")
+                                    os.system("cls")
+
+                    else:
+                        print("Nenhum cadastro realizado")
+
+                        os.system("pause")
+                        os.system("cls")
 
                 case 2:
                     poupança(user)
