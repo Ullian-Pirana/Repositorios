c6ca264e09f90293dd5436c86a6ce56d89e5772d
Desocupad0 <senaiullian@gmail.com>
2024-10-04 08:17:54 -0300

    Organizando o código principal

--- 2º Semestre/atividade 6 (Veiculos)/Main.py
+++ 2º Semestre/atividade 6 (Veiculos)/Main.py
@@ -1,5 +1,8 @@
+#Principal
 import os
 from Defs import *
+from Listas import *
+from Classes import *
 
 while True:
     os.system("cls")
@@ -7,5 +10,12 @@ while True:
 
     match escolha:
         case 1:
-            pass 
+            user = login(cliente)
+        case 2:
+            cliente.append(cliente())
+        case 3:
+            veículo.append(cadastrar_veiculo())
+        case 4:
+            pass
+            
         
