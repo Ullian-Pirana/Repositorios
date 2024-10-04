5dce3ea4da92b21ed6ba63d1bc86875ebdb59c37
Desocupad0 <senaiullian@gmail.com>
2024-10-04 08:17:03 -0300

    Adição de novas listas

--- 2º Semestre/atividade 6 (Veiculos)/Listas.py
+++ 2º Semestre/atividade 6 (Veiculos)/Listas.py
@@ -1,7 +1,20 @@
+# Listas(Armazenamnento de dados)
+
+#Todas as informações sobre "carro", seram armazenadas nessa lista
 carros = []
 
+#Todas as informações sobre "moto", seram armazenadas nessa lista
 motos = []
 
+#Todas as informações sobre os clientes, seram armazenadas nessa lista
 cliente = []
 
+#Todas as informações sobre os funcionários, seram armazenadas nessa lista
 funcionario = []
+
+#Todas as informações sobre placas dos veículos, seram armazenadas nessa lista
+placas = []
+
+cpfs = []
+
+veículo = []
