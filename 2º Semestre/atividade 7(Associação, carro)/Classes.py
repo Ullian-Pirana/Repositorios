dd14162ee0ca1c48bfcf199dda8425e0f4240845
Desocupad0 <senaiullian@gmail.com>
2024-10-04 14:19:59 -0300

    criação de todas as classes

---
+++ 2º Semestre/atividade 7(Associação, carro)/Classes.py
@@ -0,0 +1,54 @@
+class Motor:
+
+    def __init__(self, potencia : float, marca: str):
+        self.__potencia = potencia
+        self.__marca = marca
+
+    def getPotencia(self):
+        return self.__potencia
+    
+    def setPotencia(self, pot: float):
+        self.__potencia = pot
+        
+    def getMarca(self):
+        return self.__marca
+    
+    def setMarca(self, marca: str):
+        self.__marca = marca
+
+class Rodas:
+
+    def __init__(self, tamanho : int, marca: str):
+        self.__tamanho = tamanho
+        self.__marca = marca
+
+    def getTamanho(self):
+        return self.__tamanho
+    
+    def setTamanho(self, tamanho: float):
+        self.__tamanho = tamanho
+        
+    def getMarca(self):
+        return self.__marca
+    
+    def setMarca(self, marca: str):
+        self.__marca = marca
+
+class Carro:
+
+    def __init__(self, motor, rodas):
+        self.__motor = motor
+        self.__rodas = rodas
+
+    def getMotor(self):
+        return self.__motor
+    
+    def setMotor(self, motor):
+        self.__motor = motor
+        
+    def getRodas(self):
+        return self.__rodas
+    
+    def setRodas(self, rodas):
+        self.__rodas = rodas
+
