34e7bdcca4a97f7a572e825f5039f165ca0b3545
Desocupad0 <senaiullian@gmail.com>
2024-10-25 13:21:58 -0300

    def menu

--- 2º Semestre/atividade 11 (Projeto Final - Banco)/Defis.py
+++ 2º Semestre/atividade 11 (Projeto Final - Banco)/Defis.py
@@ -2,4 +2,32 @@ import os
 from classis import *
 
 def menu():
+
+    print("O que gostaria de fazer?\n \t 1 - Login\n \t 2 - Cadastrar-se \n \t 3 - Excluir Conta ")
+
+    while True:
+        try:
+            escolha =  int(input("--> "))
+
+            os.system("cls")
+            break
+
+        except Exception as e:                      
+            print(f"Impossivel concluir operação \n erro encontrado: {e}")
+
+            os.system("pause")
+            os.system("cls")
     
+    return escolha
+
+
+
+
+
+
+
+
+
+
+
+
