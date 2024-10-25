947c375881c4cee073e0d981a798f3c8353a880c
Desocupad0 <senaiullian@gmail.com>
2024-10-25 14:13:10 -0300

    def de cadastro, exclusão e login de usuarios

--- 2º Semestre/atividade 11 (Projeto Final - Banco)/Defis.py
+++ 2º Semestre/atividade 11 (Projeto Final - Banco)/Defis.py
@@ -2,10 +2,10 @@ import os
 from classis import *
 
 def menu():
-
-    print("O que gostaria de fazer?\n \t 1 - Login\n \t 2 - Cadastrar-se \n \t 3 - Excluir Conta ")
-
     while True:
+        print("\t ~~~~Ull Bank~~~ \n")
+        print("O que gostaria de fazer?\n \t 1 - Login\n \t 2 - Cadastrar-se \n \t 3 - Excluir Conta ")
+
         try:
             escolha =  int(input("--> "))
 
@@ -20,9 +20,101 @@ def menu():
     
     return escolha
 
+def cadastro(banco):
+    while True:
+        print("\t Cadastro \n")
+        print("Para realizarmos seu cadastro vamos precisar\n das seguintes informações: ")
 
+        try:
+            nome = input("Nome: ")
+            cpf = input("Cpf: ")
 
+            cliente = Cliente(nome, cpf)
+            banco.add_cliente(cliente)
 
+            print("cadastro realizado com sucesso!")
+            os.system("cls")
+            
+            break
+
+        except Exception as e:                      
+            print(f"Impossivel concluir operação \n erro encontrado: {e}")
+
+            os.system("pause")
+            os.system("cls")  
+
+def excluir(banco):
+    while True:
+        print("\t Excluir Conta \n")
+
+        if len(banco.getClientes()) > 0:
+            print("Para realizar a exclusão da sua conta precisamos que nos de \n as seguintes informações: ")
+
+            try:
+                nome = input("Seu Nome: ")
+                cpf = input("Seu Cpf: ")
+
+                for cliente in banco.getClientes():
+                    if nome == cliente.getNome() and cpf == cliente.getCpf():
+                        banco.rmv_cliente(cliente)
+
+                        print("Exclusão realizada com êxito!")
+
+                        os.system("pause")
+                        os.system("cls")
+                        break 
+
+                    else:
+                        print("Nenhum cliente encontrado com essas informações...")
+                        os.system("pause")
+                        os.system("cls")
+
+            except Exception as e:                      
+                print(f"Impossivel concluir operação \n erro encontrado: {e}")
+
+                os.system("pause")
+                os.system("cls")
+
+        else:
+            print("Nenhum Cadastro encontrado...")
+            os.system("pause")
+            os.system("cls")
+
+            break
+
+def login(banco):
+    while True:
+        print("\t _-_-Login-_-_ \n")
+        print("Por favor, insira suas informações de login:")
+
+        try:
+            nome = input("Nome: ")
+            cpf = input("Cpf: ")
+
+            cliente_encontrado = None
+
+            for cliente in banco.getClientes():
+                if nome == cliente.getNome() and cpf == cliente.getCpf():
+                    cliente_encontrado = cliente
+
+                    print(f"Login bem-sucedido! Bem-vindo, {cliente_encontrado.getNome()}!")
+                    os.system("pause")
+                    os.system("cls")
+
+                    return cliente_encontrado
+
+                    break
+
+                else:
+                    print("Nome ou CPF inválidos. Tente novamente.")
+                    os.system("pause")
+                    os.system("cls")
+
+        except Exception as e:
+            print(f"Impossível concluir a operação \n erro encontrado: {e}")
+            
+            os.system("pause")
+            os.system("cls")
 
 
 
