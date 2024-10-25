948a563f3caf22659f9d6e613c7858db3fd8f0a6
Desocupad0 <senaiullian@gmail.com>
2024-10-25 16:17:32 -0300

    povoação inicial do main

--- 2º Semestre/atividade 11 (Projeto Final - Banco)/UllBank.py
+++ 2º Semestre/atividade 11 (Projeto Final - Banco)/UllBank.py
@@ -1,3 +1,15 @@
 import os
 from classis import *
 from Defis import *
+
+Ull = Banco()
+
+while True:
+        escolha = menu () 
+
+        match escolha: 
+                case 1:
+                    login(Ull)
+                
+                case 2:
+                      cadastro(Ull)
