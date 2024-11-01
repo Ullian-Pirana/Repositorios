e81f461f4811b7c1fa5b4ad6a24e7b7cc4b7bc83
Desocupad0 <senaiullian@gmail.com>
2024-11-01 10:03:42 -0300

    upgrade

--- 2º Semestre/atividade 11 (Projeto Final - Banco)/UllBank.py
+++ 2º Semestre/atividade 11 (Projeto Final - Banco)/UllBank.py
@@ -5,27 +5,41 @@ from Defis import *
 Ull = Banco()
 
 while True:
-        escolha = menu () 
+    escolha = menu () 
 
-        match escolha: 
-                case 1:
-                    usuario = login(Ull)
-                    conta = tela_banco(usuario)
+    match escolha: 
+        case 1:
+            usuario = login(Ull)
+            conta = tela_banco(usuario)
 
-                    while True:
-                        match conta:
-                            case 1:
-                                conta_corrente(usuario)
-                            case 2:
-                                conta_poupanca(usuario)
-                            case 3:
-                                print("Saindo...")
-                                os.system("pause")
-                                os.system("cls")
-                                break
-                            case _:
-                                    print("opção invalida...")
-                                    os.system("pause")
-                                    os.system("cls")
-                case 2:
-                    cadastro(Ull)
+            while True:
+                match conta:
+                    case 1:
+                        conta_corrente(usuario)
+                    case 2:
+                        conta_poupanca(usuario)
+                    case 3:
+                        print("Saindo...")
+                        os.system("pause")
+                        os.system("cls")
+                        break
+                    case _:
+                            print("opção invalida...")
+                            os.system("pause")
+                            os.system("cls")
+        case 2:
+            cadastro(Ull)
+
+        case 3:
+            excluir(Ull)
+        
+        case 4:
+            print("Saindo...")
+            os.system("pause")
+            os.system("cls")
+            break
+
+        case _:
+            print("opção invalida...")
+            os.system("pause")
+            os.system("cls")
