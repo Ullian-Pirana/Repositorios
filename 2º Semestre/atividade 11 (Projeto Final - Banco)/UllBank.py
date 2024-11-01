d777cf504a204bae5d8c189e26f0b87139ba100f
Desocupad0 <senaiullian@gmail.com>
2024-11-01 09:50:10 -0300

    1º finalização do arquivo main

--- 2º Semestre/atividade 11 (Projeto Final - Banco)/UllBank.py
+++ 2º Semestre/atividade 11 (Projeto Final - Banco)/UllBank.py
@@ -9,7 +9,24 @@ while True:
 
         match escolha: 
                 case 1:
-                    usario = login(Ull)
+                    usuario = login(Ull)
+                    conta = tela_banco(usuario)
+
+                    while True:
+                        match conta:
+                            case 1:
+                                conta_corrente(usuario)
+                            case 2:
+                                conta_poupanca(usuario)
+                            case 3:
+                                print("Saindo...")
+                                os.system("pause")
+                                os.system("cls")
+                                break
+                            case _:
+                                    print("opção invalida...")
+                                    os.system("pause")
+                                    os.system("cls")
                 
                 case 2:
                     cadastro(Ull)
