1b5fee7422192135d972b17582d83881cc79d9db
Ullian-Pirana <176522532+Ullian-Pirana@users.noreply.github.com>
2025-05-21 16:02:05 -0300

    exclusão de outro projeto DJango repetido

--- 3º Semestre/Atividade 12 (Hotel Aria) remaster/Hotelaria/Hotel/forms.py
+++
@@ -1,7 +0,0 @@
-from django import forms
-from .models import quarto
-
-class quartoForms(forms.ModelForm):
-    class Meta:
-        model = quarto
-        fields = ['num_Quarto','qtd_Hospedes','tipo','valor','descricao','status', 'img']
--- 3º Semestre/Atividade 12 (Hotel Aria) remaster/Hotelaria/Hotel/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path, include
-from . import views
-
-urlpatterns = [
-    path('', views.Homepage, name='homepage'),
-    path('login', views.Login, name='login'),
-    path('logout', views.Sair, name='logout'),
-    path('addQuarto', views.addQuarto, name="addQuarto")
-]
