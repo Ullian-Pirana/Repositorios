2e128a60196dc7af1df5478674d00d5a97ed52b6
Ullian-Pirana <176522532+Ullian-Pirana@users.noreply.github.com>
2025-05-21 14:45:34 -0300

    povoação inicial do projeto DJango

--- 3º Semestre/Atividade 11 (Hotel Aria)/Hotelaria/Hotel/models.py
+++ 3º Semestre/Atividade 11 (Hotel Aria)/Hotelaria/Hotel/models.py
@@ -1,3 +1,38 @@
 from django.db import models
 
 # Create your models here.
+class homepage(models.Model):
+    titulo = models.CharField(max_length=50)
+    descricao = models.TextField(max_length=200)
+    logo = models.ImageField(upload_to='homepage/')
+
+    def __str__(self):
+        return self.titulo
+    
+
+class quarto(models.Model):
+
+    tipo_quarto = [
+        ("Normal", "Normal"),
+        ("Confort","Confort"),
+        ("Plus", "Plus"),
+        ("Premium", "Premium"),
+    ]
+    estilo_quarto = [
+        ("Solteiro", "Solteiro"),
+        ("Casal", "Casal"),
+        ("Familia", "Familia")
+    ]
+    status_status = [
+        (1, "Disponivel"),
+        (0, "Reservado")
+    ]
+
+    num_Quarto = models.IntegerField()
+    qtd_Hospedes = models.IntegerField()
+    estilo = models.CharField(choices=estilo_quarto)
+    tipo = models.CharField(choices=tipo_quarto)
+    valor = models.FloatField(max_length=3)
+    descricao = models.TextField(max_length=500)
+    status = models.BooleanField(choices=status_status, default=1)
+    img = models.ImageField(upload_to='quarto/')
--- 3º Semestre/Atividade 11 (Hotel Aria)/Hotelaria/Hotel/urls.py
+++ 3º Semestre/Atividade 11 (Hotel Aria)/Hotelaria/Hotel/urls.py
@@ -1,6 +1,9 @@
-from django.urls import path
+from django.urls import path, include
 from . import views
 
 urlpatterns = [
-    path("", views.home, name="homepage"),
+    path('', views.Homepage, name='homepage'),
+    path('login', views.Login, name='login'),
+    path('logout', views.Sair, name='logout'),
+    path('addQuarto', views.addQuarto, name="addQuarto")
 ]
