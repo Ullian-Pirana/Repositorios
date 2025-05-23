c78c18d864bb92d4253e5135b31da3c8e4ead6a9
Ullian-Pirana <176522532+Ullian-Pirana@users.noreply.github.com>
2025-05-23 13:53:24 -0300

    re-estilização do forms de add_quarto para que o campo de status volte a ser um dropdown(select)

--- 3º Semestre/Atividade 11 (Hotel Aria)/Hotelaria/Hotel/forms.py
+++ 3º Semestre/Atividade 11 (Hotel Aria)/Hotelaria/Hotel/forms.py
@@ -1,7 +1,17 @@
 from django import forms
 from .models import quarto
 
+from django import forms
+from .models import quarto
+
 class quartoForms(forms.ModelForm):
+    status_escolha = [
+        (True, 'Disponível'),
+        (False, 'Reservado'),
+    ]
+
+    status = forms.ChoiceField(choices=status_escolha, widget=forms.Select())
+
     class Meta:
         model = quarto
-        fields = ['num_Quarto','qtd_Hospedes','estilo','tipo','valor','descricao','status', 'img']
+        fields = ['num_Quarto', 'qtd_Hospedes', 'estilo', 'tipo', 'valor', 'descricao', 'status', 'img']
